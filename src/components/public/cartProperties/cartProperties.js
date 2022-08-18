ded4e756a0b7c6e9e7262d1729356dd31706a8fb
tsago123 <zaal.tsagareli.1@btu.edu.ge>
2022-08-18 15:23:50 +0400

    x

--- src/components/public/cartProperties/cartProperties.js
+++ src/components/public/cartProperties/cartProperties.js
@@ -1,15 +1,5 @@
 import React from "react";
-import classes from "./Cart.module.css";
-import { connect } from "react-redux";
-import TotalAmount from "./CartComponents/totalAmount/totalAmount";
-import fetch from "../helper/fetch";
-import { getSingleProduct } from "../Schemas/getSingleProduct";
-import Modal from "../Modal/Modal";
-import ChekoutButton from './CartComponents/actions/checkoutButton'
-import ViewBagButton from "./CartComponents/actions/viewBagButton";
-import CartProducts from "./CartComponents/products/product";
-import { cartSliceActions } from "../../store/cartSlice";
-import TotalPrice from "./CartComponents/totalPrice/totalPrice";
+
 class CartProperties  extends React.Component{
 
 }
